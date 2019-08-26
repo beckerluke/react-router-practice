dc7448de62f3dd5d78c230808f12f1f1105e92e2
Luke Becker <beckerluke1@gmail.com>
2019-08-26 16:35:23 -0500

    Created Home, Portfolio, and Contact components and created link and route path for each in App

---
+++ src/components/App/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import Portfolio from '../Portfolio/Portfolio';
+import Home from '../Home/Home';
+import Contact from '../Contact/Contact';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
